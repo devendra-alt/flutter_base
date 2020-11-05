a236b0d287f7bff7aadf40ff6ce72095d6c80150
devendra mulewa <devendramulewa171@gmail.com>
2020-11-05 10:08:32 +0530

    ninja02

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Text('my first widget'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('my app'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('body center'),
+        ),
+        backgroundColor: Colors.black87,
+        floatingActionButton: FloatingActionButton(
+          child: Text('click'),
+          onPressed: null,
+        ),
+      ),
     ));
