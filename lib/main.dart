07c6daad25ca579546ab1c68f6a563683fbfc1ae
devendra mulewa <devendramulewa171@gmail.com>
2020-11-05 16:06:01 +0530

    ninja04

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,31 @@
+import 'dart:typed_data';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Text('my first widget'),
-    ));
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('hey buddy'),
+        backgroundColor: Colors.amberAccent,
+      ),
+      body: Center(
+        child: Text(
+          'in the app',
+          style: TextStyle(
+            color: Colors.red,
+            fontSize: 40.0,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+      ),
+    );
+  }
+}
