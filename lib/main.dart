c45500a6e53e308d9c7957d8050031c005ff9ad1
devendra mulewa <devendramulewa171@gmail.com>
2020-11-05 16:44:50 +0530

    dele

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Text('my first widget'),
-    ));
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'my app',
+          style: TextStyle(fontFamily: 'NotoSans'),
+        ),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage(''),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.red,
+        child: Text('click'),
+      ),
+    );
+  }
+}
